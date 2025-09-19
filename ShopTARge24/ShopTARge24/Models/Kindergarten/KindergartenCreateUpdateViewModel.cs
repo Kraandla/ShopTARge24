7266d189c003c44ffde73923c61e8360e4e36716
Kraandla <kevinrandla22@gmail.com>
2025-09-19 13:13:46 +0300

    View models and Index page
    Added code to the View models and created the index page layout. Added Kindergarten tab to the navbar. Created empty Kindergarten services and controller

--- ShopTARge24/ShopTARge24/Models/Kindergarten/KindergartenCreateUpdateViewModel.cs
+++ ShopTARge24/ShopTARge24/Models/Kindergarten/KindergartenCreateUpdateViewModel.cs
@@ -2,5 +2,12 @@
 {
     public class KindergartenCreateUpdateViewModel
     {
+            public Guid? Id { get; set; }
+            public string? KindergartenName { get; set; }
+            public string? GroupName { get; set; }
+            public string? TeacherName { get; set; }
+            public int? ChildCount { get; set; }
+            public DateTime? CreatedAt { get; set; }
+            public DateTime? UpdatedAt { get; set; }
     }
 }
