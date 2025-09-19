090ab3467408947f9169ef8b888d240a69fc2a5d
Kraandla <kevinrandla22@gmail.com>
2025-09-19 14:26:47 +0300

    Index view
    Created index view for kindergarten page

--- ShopTARge24/ShopTARge24/Controllers/KindergartenController.cs
+++ ShopTARge24/ShopTARge24/Controllers/KindergartenController.cs
@@ -1,12 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ShopTARge24.Core.ServiceInterface;
+using ShopTARge24.Data;
+using ShopTARge24.Models.Kindergarten;
+using ShopTARge24.Models.Spaceships;
 
 namespace ShopTARge24.Controllers
 {
     public class KindergartenController : Controller
     {
+        private readonly ShopTARge24Context _context;
+        private readonly IKindergartenServices _KindergartenServices;
+
+        public KindergartenController
+            (
+                ShopTARge24Context context,
+                IKindergartenServices KindergartenServices
+            )
+        {
+            _context = context;
+            _KindergartenServices = KindergartenServices;
+        }
+
+
         public IActionResult Index()
         {
-            return View();
+            var result = _context.Kindergartens
+                .Select(x => new KindergartenIndexViewModel
+                {
+                    Id = x.Id,
+                    KindergartenName = x.KindergartenName,
+                    GroupName = x.GroupName,
+                    ChildCount = x.ChildCount,
+                    CreatedAt = x.CreatedAt,
+                });
+
+            return View(result);
         }
     }
 }
--- ShopTARge24/ShopTARge24/Models/Kindergarten/KindergartenIndexViewModel.cs
+++ ShopTARge24/ShopTARge24/Models/Kindergarten/KindergartenIndexViewModel.cs
@@ -5,9 +5,7 @@
         public Guid? Id { get; set; }
         public string? KindergartenName { get; set; }
         public string? GroupName { get; set; }
-        public string? TeacherName { get; set; }
         public int? ChildCount { get; set; }
         public DateTime? CreatedAt { get; set; }
-        public DateTime? UpdatedAt { get; set; }
     }
 }
