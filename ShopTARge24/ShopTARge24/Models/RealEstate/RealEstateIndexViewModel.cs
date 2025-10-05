0edfc513cdd9160225390aff71fbe888d6ecf716
Kraandla <kevinrandla22@gmail.com>
2025-10-05 14:25:52 +0300

    Index view model

---
+++ ShopTARge24/ShopTARge24/Models/RealEstate/RealEstateIndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace ShopTARge24.Models.RealEstate
+{
+    public class RealEstateIndexViewModel
+    {
+        public Guid? Id { get; set; }
+
+        public string? Area { get; set; }
+
+        public string? Location { get; set; }
+
+        public int? RoomNumber { get; set; }
+
+        public string? BuildingType { get; set; }
+    }
+}
