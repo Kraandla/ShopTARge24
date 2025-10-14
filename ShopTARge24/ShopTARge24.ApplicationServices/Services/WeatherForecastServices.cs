e8b34526a34fa63021d1bd3aca05eb2d01c6a7e9
Kraandla <kevinrandla22@gmail.com>
2025-10-14 08:21:56 +0300

    AccuLocation

--- ShopTARge24/ShopTARge24.ApplicationServices/Services/WeatherForecastServices.cs
+++ ShopTARge24/ShopTARge24.ApplicationServices/Services/WeatherForecastServices.cs
@@ -1,11 +1,12 @@
 ﻿using ShopTARge24.Core.Dto;
 using ShopTARge24.Core.ServiceInterface;
+using System.Text.Json;
 
 namespace ShopTARge24.ApplicationServices.Services
 {
     public class WeatherForecastServices : IWeatherForecast
     {
-        public async Task<AccuLocationWeatherResultDto> AccuWeatherResult(AccuLocationWeatherResultDto dto)
+        public async Task<AccuLocationRootDto> AccuWeatherResult(AccuLocationRootDto dto)
         {
             //Tallinna linnakood 127964
             var response = $"https://dataservice.accuweather.com/locations/v1/cities/search";
@@ -14,7 +15,11 @@ namespace ShopTARge24.ApplicationServices.Services
             {
                 var httpResponse = await client.GetAsync(response);
                 string json = await httpResponse.Content.ReadAsStringAsync();
+
+                List<AccuLocationRootDto> weatherData = JsonSerializer.Deserialize<List<AccuLocationRootDto>>(json);
             }
+
+            return dto;
         }
     }
 }
---
+++ ShopTARge24/ShopTARge24.Core/Dto/AccuLocationRootDto.cs
@@ -0,0 +1,32 @@
+﻿namespace ShopTARge24.Core.Dto
+{
+
+    public class AccuLocationRootDto
+        {
+            public string? LocalObservationDateTime { get; set; }
+            public long EpochTime { get; set; }
+            public string? WeatherText { get; set; }
+            public int WeatherIcon { get; set; }
+            public bool HasPrecipitation { get; set; }
+            public string? PrecipitationType { get; set; }
+            public bool IsDayTime { get; set; }
+
+            public string? MobileLink { get; set; }
+            public string? Link { get; set; }
+
+            public class Temperature
+            {
+                public UnitValue? Metric { get; set; }
+                public UnitValue? Imperial { get; set; }
+            }
+
+            public class UnitValue
+            {
+                public double Value { get; set; }
+                public string? Unit { get; set; }
+                public int UnitType { get; set; }
+            }
+        }
+
+}
+
