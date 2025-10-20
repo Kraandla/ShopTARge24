e5ec87d93f3467e171c8dc4228b4ee07c678e655
Kraandla <kevinrandla22@gmail.com>
2025-10-20 09:00:24 +0300

    City code layout change

--- ShopTARge24/ShopTARge24.Core/Dto/AccuCityCodeRootDto.cs
+++ ShopTARge24/ShopTARge24.Core/Dto/AccuCityCodeRootDto.cs
@@ -1,94 +1,94 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace ShopTARge24.Core.Dto
+﻿namespace ShopTARge24.Core.Dto
 {
-    public class AdministrativeArea
+    public class AccuCityCodeRootDto
     {
-        public string ID { get; set; }
-        public string LocalizedName { get; set; }
-        public string EnglishName { get; set; }
-        public int Level { get; set; }
-        public string LocalizedType { get; set; }
-        public string EnglishType { get; set; }
-        public string CountryID { get; set; }
+        public CityCode[]? CityCode { get; set; }
     }
 
-    public class Country
+    public class CityCode
     {
-        public string ID { get; set; }
-        public string LocalizedName { get; set; }
-        public string EnglishName { get; set; }
-    }
-
-    public class Elevation
-    {
-        public Metric Metric { get; set; }
-        public Imperial Imperial { get; set; }
-    }
-
-    public class GeoPosition
-    {
-        public double Latitude { get; set; }
-        public double Longitude { get; set; }
-        public Elevation Elevation { get; set; }
-    }
-
-    public class Imperial
-    {
-        public double Value { get; set; }
-        public string Unit { get; set; }
-        public int UnitType { get; set; }
-    }
-
-    public class Metric
-    {
-        public double Value { get; set; }
-        public string Unit { get; set; }
-        public int UnitType { get; set; }
+        public int Version { get; set; }
+        public string Key { get; set; } = string.Empty;
+        public string Type { get; set; } = string.Empty;
+        public int Rank { get; set; }
+        public string LocalizedName { get; set; } = string.Empty;
+        public string EnglishName { get; set; } = string.Empty;
+        public string PrimaryPostalCode { get; set; } = string.Empty;
+        public Region? Region { get; set; }
+        public Country? Country { get; set; }
+        public AdministrativeArea? AdministrativeArea { get; set; }
+        public Timezone? TimeZone { get; set; }
+        public Geoposition? GeoPosition { get; set; }
+        public bool IsAlias { get; set; }
+        public SupplementalAdminArea[]? SupplementalAdminAreas { get; set; }
+        public string[]? DataSets { get; set; }
     }
 
     public class Region
     {
-        public string ID { get; set; }
-        public string LocalizedName { get; set; }
-        public string EnglishName { get; set; }
+        public string Id { get; set; } = string.Empty;
+        public string LocalizedName { get; set; } = string.Empty;
+        public string EnglishName { get; set; } = string.Empty;
     }
 
-    public class Root
+    public class Country
     {
-        public int Version { get; set; }
-        public string Key { get; set; }
-        public string Type { get; set; }
-        public int Rank { get; set; }
-        public string LocalizedName { get; set; }
-        public string EnglishName { get; set; }
-        public string PrimaryPostalCode { get; set; }
-        public Region Region { get; set; }
-        public Country Country { get; set; }
-        public AdministrativeArea AdministrativeArea { get; set; }
-        public TimeZone TimeZone { get; set; }
-        public GeoPosition GeoPosition { get; set; }
-        public bool IsAlias { get; set; }
-        public List<SupplementalAdminArea> SupplementalAdminAreas { get; set; }
-        public List<string> DataSets { get; set; }
+        public string Id { get; set; } = string.Empty;
+        public string LocalizedName { get; set; } = string.Empty;
+        public string EnglishName { get; set; } = string.Empty;
+    }
+
+    public class AdministrativeArea
+    {
+        public string Id { get; set; } = string.Empty;
+        public string LocalizedName { get; set; } = string.Empty;
+        public string EnglishName { get; set; } = string.Empty;
+        public int Level { get; set; }
+        public string LocalizedType { get; set; } = string.Empty;
+        public string EnglishType { get; set; } = string.Empty;
+        public string CountryID { get; set; } = string.Empty;
+    }
+
+    public class Timezone
+    {
+        public string Code { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public int GmtOffset { get; set; }
+        public bool IsDaylightSaving { get; set; }
+        public DateTime NextOffsetChange { get; set; }
+    }
+
+    public class Geoposition
+    {
+        public float Latitude { get; set; }
+        public float Longitude { get; set; }
+        public Elevation? Elevation { get; set; }
+    }
+
+    public class Elevation
+    {
+        public Metric? Metric { get; set; }
+        public Imperial? Imperial { get; set; }
+    }
+
+    public class Metric
+    {
+        public int Value { get; set; }
+        public string Unit { get; set; } = string.Empty;
+        public int UnitType { get; set; }
+    }
+
+    public class Imperial
+    {
+        public int Value { get; set; }
+        public string Unit { get; set; } = string.Empty;
+        public int UnitType { get; set; }
     }
 
     public class SupplementalAdminArea
     {
         public int Level { get; set; }
-        public string LocalizedName { get; set; }
-        public string EnglishName { get; set; }
+        public string LocalizedName { get; set; } = string.Empty;
+        public string EnglishName { get; set; } = string.Empty;
     }
-
-    public class TimeZone
-    {
-        public string Code { get; set; }
-        public string Name { get; set; }
-        public double GmtOffset { get; set; }
-        public bool IsDaylightSaving { get; set; }
-        public DateTime NextOffsetChange { get; set; }
-    }
-
-
 }
