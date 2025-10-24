47ec87414a511561b1d3673dad69cdcfce56652b
Kraandla <kevinrandla22@gmail.com>
2025-10-25 00:02:54 +0300

    ChuckAPI call and view

---
+++ ShopTARge24/ShopTARge24.ApplicationServices/Services/ChuckNorrisService.cs
@@ -0,0 +1,37 @@
+﻿using ShopTARge24.Core.ServiceInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ShopTARge24.ApplicationServices.Services
+{
+    public class ChuckNorrisService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ChuckNorrisService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<string> GetRandomChuckJoke() {
+            var response = await _httpClient.GetAsync("https://api.chucknorris.io/jokes/random");
+
+            using var stream = await response.Content.ReadAsStreamAsync();
+            var jokeData = await JsonSerializer.DeserializeAsync<ChuckJoke>(
+                stream,
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true }
+            );
+
+            return jokeData?.value;
+        }
+    }
+
+    public class ChuckJoke
+    {
+        public string? value {  get; set; }
+    }
+}
---
+++ ShopTARge24/ShopTARge24/Controllers/ChuckJokeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopTARge24.ApplicationServices.Services;
+
+namespace ShopTARge24.Controllers
+{
+    public class ChuckJokeController : Controller
+    {
+        private readonly ChuckNorrisService _chuckNorrisService;
+
+        public ChuckJokeController(ChuckNorrisService chuckNorrisService)
+        {
+            _chuckNorrisService = chuckNorrisService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var joke = await _chuckNorrisService.GetRandomChuckJoke();
+            ViewData["Joke"] = joke;
+            return View();
+        }
+    }
+}
