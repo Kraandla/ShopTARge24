79f5c712bfee94eb98fda6a1b50fe6d4082407b6
Kraandla <kevinrandla22@gmail.com>
2025-11-04 09:47:59 +0200

    Add OpenWeather service dependency to controller
    Injected IOpenWeatherServices into OpenWeatherController for future weather-related functionality. Also added a folder for OpenWeather models in the project file.

--- ShopTARge24/ShopTARge24/Controllers/OpenWeatherController.cs
+++ ShopTARge24/ShopTARge24/Controllers/OpenWeatherController.cs
@@ -1,9 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ShopTARge24.Core.ServiceInterface;
 
 namespace ShopTARge24.Controllers
 {
     public class OpenWeatherController : Controller
     {
+        private readonly IOpenWeatherServices _openWeatherServices;
+        public OpenWeatherController(
+            IOpenWeatherServices openWeatherServices
+            )
+        {
+            _openWeatherServices = openWeatherServices;
+        }
+
         public IActionResult Index()
         {
             return View();
